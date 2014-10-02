4ccfa24e9765b67e56678e7a22a4d420e2a3e51d
Luke Daigneault <luke.daigneault@gmail.com>
2014-10-02 15:43:16 +0200

    Fixed collapsing

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -60,9 +60,8 @@
 		 <!-- Bootstrap core JavaScript
 		 ================================================== -->
 		 <!-- Placed at the end of the document so the pages load faster -->
-			 
-		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 		<script src="{{ asset('js/jquery-1.11.1.min.js') }}"></script>
+		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 		@yield('scripts')
 	</body>
 </html>
