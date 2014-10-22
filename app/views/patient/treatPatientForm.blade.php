b2155c494a20a3c65d70a7f0d647312aee243f8e
Luke Daigneault <luke.daigneault@gmail.com>
2014-10-22 09:45:25 +0200

    multi column css fix

--- app/views/patient/treatPatientForm.blade.php
+++ app/views/patient/treatPatientForm.blade.php
@@ -30,27 +30,29 @@
 	<strong>Social History:</strong> {{ $patient->history->social }}<br/>
 	<strong>Drug History :</strong> {{ $patient->history->drug }}<br/>
 	<strong>Pathologies :</strong><br/>
-	<ul>
-		{{ ($patient->history->diplopia) ? '<li>Diplopia</li>' : null  }} 
-		{{ ($patient->history->dizziness) ? '<li>Dizziness</li>' : null  }}  
-		{{ ($patient->history->speechSwallow) ? '<li>Speech/Swallow</li>' : null  }}
-		{{ ($patient->history->blackouts) ? '<li>Blackouts</li>' : null  }}  
-		{{ ($patient->history->pacemaker) ? '<li>Pacemaker</li>' : null  }}  
-		{{ ($patient->history->bilateralNeuroSigns) ? '<li>Bilateral Neuro Signs</li>' : null  }}  
-		{{ ($patient->history->saddleAnaesthesia) ? '<li>Saddle Anaesthesia</li>' : null  }}  
-		{{ ($patient->history->cancerHistory) ? '<li>Cancer History</li>' : null  }}  
-		{{ ($patient->history->weightloss) ? '<li>Weightloss</li>' : null  }}  
-		{{ ($patient->history->steroids) ? '<li>Steroids</li>' : null  }}  
-		{{ ($patient->history->anticoagulants) ? '<li>Anticoagulants</li>' : null  }}  
-		{{ ($patient->history->pregnant) ? '<li>Pregnant</li>' : null  }}  
-		{{ ($patient->history->diabetes) ? '<li>Diabetes</li>' : null  }}  
-		{{ ($patient->history->epilepsy) ? '<li>Epilepsy</li>' : null  }}  
-		{{ ($patient->history->bloodPressure) ? '<li>Blood pressure</li>' : null  }}  
-		{{ ($patient->history->heartConditions) ? '<li>Heart Conditions</li>' : null  }}  
-		{{ ($patient->history->osteoporosis) ? '<li>Osteoporosis</li>' : null  }}  
-		{{ ($patient->history->thyroid) ? '<li>Thyroid</li>' : null  }}  
-		{{ ($patient->history->arthritis) ? '<li>Arthritis</li>' : null  }}
-	</ul>
+	<div id="multiColumn">
+		<ul>
+			{{ ($patient->history->diplopia) ? '<li>Diplopia</li>' : null  }} 
+			{{ ($patient->history->dizziness) ? '<li>Dizziness</li>' : null  }}  
+			{{ ($patient->history->speechSwallow) ? '<li>Speech/Swallow</li>' : null  }}
+			{{ ($patient->history->blackouts) ? '<li>Blackouts</li>' : null  }}  
+			{{ ($patient->history->pacemaker) ? '<li>Pacemaker</li>' : null  }}  
+			{{ ($patient->history->bilateralNeuroSigns) ? '<li>Bilateral Neuro Signs</li>' : null  }}  
+			{{ ($patient->history->saddleAnaesthesia) ? '<li>Saddle Anaesthesia</li>' : null  }}  
+			{{ ($patient->history->cancerHistory) ? '<li>Cancer History</li>' : null  }}  
+			{{ ($patient->history->weightloss) ? '<li>Weightloss</li>' : null  }}  
+			{{ ($patient->history->steroids) ? '<li>Steroids</li>' : null  }}  
+			{{ ($patient->history->anticoagulants) ? '<li>Anticoagulants</li>' : null  }}  
+			{{ ($patient->history->pregnant) ? '<li>Pregnant</li>' : null  }}  
+			{{ ($patient->history->diabetes) ? '<li>Diabetes</li>' : null  }}  
+			{{ ($patient->history->epilepsy) ? '<li>Epilepsy</li>' : null  }}  
+			{{ ($patient->history->bloodPressure) ? '<li>Blood pressure</li>' : null  }}  
+			{{ ($patient->history->heartConditions) ? '<li>Heart Conditions</li>' : null  }}  
+			{{ ($patient->history->osteoporosis) ? '<li>Osteoporosis</li>' : null  }}  
+			{{ ($patient->history->thyroid) ? '<li>Thyroid</li>' : null  }}  
+			{{ ($patient->history->arthritis) ? '<li>Arthritis</li>' : null  }}
+		</ul>
+	</div>
 		<a href="{{ route('edit.history', $patient->id) }}" class="btn btn-info">Edit History</a>
 	</div>
 </div>
