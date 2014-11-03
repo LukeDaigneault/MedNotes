724ed76dbefe27b5ed211dd0645aed4b1a4c9313
Luke Daigneault <luke.daigneault@gmail.com>
2014-11-03 17:18:51 +0100

    fixed closing check mark

--- app/views/patient/treatPatientForm.blade.php
+++ app/views/patient/treatPatientForm.blade.php
@@ -5,7 +5,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             <h4 class="modal-title" id="myModalLabel">Modal title</h4>
             </div>
             <div class="modal-body">
