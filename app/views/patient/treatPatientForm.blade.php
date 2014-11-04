c996eef645ca9260a6bf5330450468580c83982a
Luke Daigneault <luke.daigneault@gmail.com>
2014-11-04 13:51:16 +0100

    removed dummy modal

--- app/views/patient/treatPatientForm.blade.php
+++ app/views/patient/treatPatientForm.blade.php
@@ -1,24 +1,6 @@
 @extends('layout')
 
 @section('content')
-<div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-            </div>
-            <div class="modal-body">
-                <h3>Modal Body</h3>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-    </div>
-  </div>
-</div>
-
 <h2 class="sub-header">Treat {{ $patient->lastName }}, {{$patient->firstName }} <small>{{ date("d/m/Y", strtotime($patient->dob)) }}</small></h2>
 <div class="row">
 	<div class="col-md-3">
@@ -79,7 +61,6 @@
 </div>
 <div class="row">
 	<div class="col-md-3">
-	<a href="#" class="btn btn-success" data-toggle="modal" data-target="#basicModal">Click to open Modal</a>
 	<a href="{{ route('edit.history', $patient->id) }}" class="btn btn-info pull-right">Edit Details</a>
 	</div>
 	<div class="col-md-3">
