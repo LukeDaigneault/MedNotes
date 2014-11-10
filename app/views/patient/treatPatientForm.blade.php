4d1610731ac5feec055c8d5c969ebda09adfd4d8
Luke Daigneault <Luke.Daigneault@gmail.com>
2014-11-10 20:58:03 +0100

    Fix for datepicker modal issue

--- app/views/patient/treatPatientForm.blade.php
+++ app/views/patient/treatPatientForm.blade.php
@@ -479,6 +479,17 @@ $(document).ready(function() {
 	
 });
 
+
+
+		// Since confModal is essentially a nested modal it's enforceFocus method
+		// must be no-op'd or the following error results 
+		// "Uncaught RangeError: Maximum call stack size exceeded"
+		// But then when the nested modal is hidden we reset modal.enforceFocus
+		var enforceModalFocusFn = $.fn.modal.Constructor.prototype.enforceFocus;
+
+		$.fn.modal.Constructor.prototype.enforceFocus = function() {};
+
+
 </script>
 
 
